f213cfc2c7daf97f787acb785575393d3d638d78
grant.robson <grant.robson@digital.hmrc.gov.uk>
2022-03-10 11:08:35 +0000

    PODS-6505 Add new repository

---
+++ app/controllers/cache/RacDacRequestsQueueEventsLogController.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.cache
+
+import com.google.inject.Inject
+import play.api.libs.json.Json
+import play.api.mvc._
+import repositories.RacDacRequestsQueueEventsLogRepository
+import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
+import uk.gov.hmrc.auth.core.{AuthConnector, AuthorisedFunctions}
+import uk.gov.hmrc.http.{HeaderCarrier, UnauthorizedException}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class RacDacRequestsQueueEventsLogController @Inject()(repository: RacDacRequestsQueueEventsLogRepository,
+                                                       val authConnector: AuthConnector,
+                                                       cc: ControllerComponents
+                                   )(implicit ec: ExecutionContext) extends BackendController(cc) with AuthorisedFunctions {
+
+  def putStatus(status: Int): Action[AnyContent] = Action.async {
+    implicit request =>
+      withId { id =>
+        repository.save(id, Json.obj("status" -> status)).map(_ => Ok)
+      }
+  }
+
+  def getStatus: Action[AnyContent] = Action.async {
+    implicit request =>
+      withId { id =>
+        repository.get(id)
+          .map {
+            case Some(jsValue) =>
+              (jsValue \ "status").asOpt[Int] match {
+                case Some(status) => Results.Status(status)
+                case _ => NotFound
+              }
+            case None => NotFound
+          }
+      }
+  }
+
+  private def withId(block: (String) => Future[Result])
+                      (implicit hc: HeaderCarrier, request: Request[AnyContent]): Future[Result] = {
+    authorised().retrieve(Retrievals.externalId) {
+      case Some(_) =>
+        hc.sessionId match {
+          case Some(sessionId) => block(sessionId.value)
+          case _ => Future.failed(SessionIdNotFound())
+        }
+      case _ => Future.failed(CredIdNotFoundFromAuth())
+    }
+  }
+}
+
+case class SessionIdNotFound(msg: String = "Session ID not found - Unable to retrieve session ID")
+  extends UnauthorizedException(msg)
---
+++ app/repositories/RacDacRequestsQueueEventsLogRepository.scala
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package repositories
+
+import com.google.inject.Inject
+import models.cache.DataJson
+import org.joda.time.{DateTime, DateTimeZone}
+import org.slf4j.{Logger, LoggerFactory}
+import play.api.Configuration
+import play.api.libs.json._
+import play.modules.reactivemongo.ReactiveMongoComponent
+import reactivemongo.api.indexes.IndexType.Ascending
+import reactivemongo.api.indexes.{Index, IndexType}
+import reactivemongo.bson.{BSONDocument, BSONObjectID}
+import reactivemongo.play.json.ImplicitBSONHandlers._
+import uk.gov.hmrc.mongo.ReactiveRepository
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class RacDacRequestsQueueEventsLogRepository @Inject()(mongoComponent: ReactiveMongoComponent,
+                                                       configuration: Configuration
+                                      )(implicit val ec: ExecutionContext)
+  extends ReactiveRepository[JsValue, BSONObjectID](
+    configuration.get[String](path = "mongodb.migration-cache.rac-dac-requests-queue-events-log.name"),
+    mongoComponent.mongoConnector.db,
+    implicitly
+  ) {
+
+  override val logger: Logger = LoggerFactory.getLogger("RacDacRequestsQueueEventsLogRepository")
+
+  private def expireInSeconds: DateTime = DateTime.now(DateTimeZone.UTC).
+    plusSeconds(configuration.get[Int](path = "mongodb.migration-cache.rac-dac-requests-queue-events-log.timeToLiveInSeconds"))
+
+  override lazy val indexes = Seq(
+    Index(Seq(("id", Ascending)), Some("id"), unique = true),
+    Index(Seq(("expireAt", IndexType.Ascending)), Some("dataExpiry"), options = BSONDocument("expireAfterSeconds" -> 0))
+  )
+
+  (for { _ <- ensureIndexes } yield { () }) recoverWith {
+    case t: Throwable => Future.successful(logger.error(s"Error creating indexes on collection ${collection.name}", t))
+  } andThen {
+    case _ => CollectionDiagnostics.logCollectionInfo(collection)
+  }
+
+  override def ensureIndexes(implicit ec: ExecutionContext): Future[Seq[Boolean]] =
+    Future.sequence(indexes.map(collection.indexesManager.ensure(_)))
+
+  def save(id: String, userData: JsValue)(implicit ec: ExecutionContext): Future[Boolean] = {
+    logger.debug("Calling Save in RacDacRequestsQueueEventsLogRepository")
+
+    val document: JsValue = Json.toJson(DataJson(id, userData, DateTime.now(DateTimeZone.UTC), expireInSeconds))
+    val modifier = BSONDocument("$set" -> document)
+
+    collection.update.one(BSONDocument("id" -> id), modifier, upsert = true).map(_.ok)
+  }
+
+  def get(id: String)(implicit ec: ExecutionContext): Future[Option[JsValue]] = {
+    logger.debug("Calling get in RacDacRequestsQueueEventsLogRepository")
+    collection.find(BSONDocument("id" -> id), projection = Option.empty[JsObject]).one[DataJson].map(_.map(_.data))
+  }
+
+  def remove(id: String)(implicit ec: ExecutionContext): Future[Boolean] = {
+    logger.warn(s"Removing row from collection ${collection.name}")
+    val selector = BSONDocument("id" -> id)
+    collection.delete.one(selector).map(_.ok)
+  }
+}
+
