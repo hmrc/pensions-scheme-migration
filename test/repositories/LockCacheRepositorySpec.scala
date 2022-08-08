43e294d7a060c6af4f33475e99a9e8ff99db8447
grant.robson <grant.robson@digital.hmrc.gov.uk>
2022-08-08 08:39:44 +0100

    PODS-7282 WIP

---
+++ test/repositories/LockCacheRepositorySpec.scala
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package repositories
+
+import com.github.simplyscala.MongoEmbedDatabase
+import models.FeatureToggle
+import models.FeatureToggleName.DummyToggle
+import models.cache.MigrationLock
+import org.mockito.MockitoSugar
+import org.mongodb.scala.model.Filters
+import org.scalatest.concurrent.ScalaFutures.whenReady
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.{BeforeAndAfter, BeforeAndAfterEach}
+import play.api.Configuration
+import uk.gov.hmrc.mongo.MongoComponent
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+
+
+class LockCacheRepositorySpec extends AnyWordSpec with MockitoSugar with Matchers with MongoEmbedDatabase with BeforeAndAfter with
+  BeforeAndAfterEach { // scalastyle:off magic.number
+
+  import LockCacheRepositorySpec._
+
+  override def beforeEach: Unit = {
+    super.beforeEach
+    when(mockConfiguration.get[String](path = "mongodb.pension-administrator-cache.admin-data.name")).thenReturn("admin-data")
+  }
+
+  withEmbedMongoFixture(port = 24680) { _ =>
+
+    "getLockByPstr" must {
+      "get lock from Mongo collection" in {
+        mongoCollectionDrop()
+
+        val documentsInDB = for {
+          _ <- repository.collection.insertOne(
+            MigrationLock()
+          )
+
+          documentsInDB <- repository.getLockByPstr("")
+        } yield documentsInDB
+
+        whenReady(documentsInDB) { documentsInDB =>
+          documentsInDB.size mustBe 1
+        }
+      }
+    }
+//
+//    "setFeatureToggle" must {
+//      "set new FeatureToggles in Mongo collection" in {
+//        mongoCollectionDrop()
+//        val documentsInDB = for {
+//          _ <- adminDataRepository.setFeatureToggles(Seq(FeatureToggle(DummyToggle, enabled = true)))
+//          documentsInDB <- adminDataRepository.collection.find[FeatureToggles](Filters.eq(id, featureToggles)).headOption()
+//        } yield documentsInDB
+//
+//        whenReady(documentsInDB) { documentsInDB =>
+//          documentsInDB.map(_.toggles.size mustBe 1)
+//        }
+//      }
+//
+//      "set empty FeatureToggles in Mongo collection" in {
+//        mongoCollectionDrop()
+//        val documentsInDB = for {
+//          _ <- adminDataRepository.setFeatureToggles(Seq.empty)
+//          documentsInDB <- adminDataRepository.collection.find[FeatureToggles].toFuture()
+//        } yield documentsInDB
+//
+//        whenReady(documentsInDB) { documentsInDB =>
+//          documentsInDB.map(_.toggles.size mustBe 0)
+//        }
+//      }
+//    }
+  }
+}
+
+
+object LockCacheRepositorySpec extends AnyWordSpec with MockitoSugar {
+
+  import scala.concurrent.ExecutionContext.Implicits._
+
+  private val mockConfiguration = mock[Configuration]
+  private val databaseName = "pension-administrator"
+  private val mongoUri: String = s"mongodb://127.0.0.1:27017/$databaseName?heartbeatFrequencyMS=1000&rm.failover=default"
+  private val mongoComponent = MongoComponent(mongoUri)
+
+  private def mongoCollectionDrop(): Void = Await
+    .result(repository.collection.drop().toFuture(), Duration.Inf)
+
+  def repository = new LockCacheRepository(mongoComponent, mockConfiguration)
+}
+
