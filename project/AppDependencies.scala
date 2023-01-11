4c8ff30d178c3b8a9a765842bf0b6429f9cf7354
cemyeniceri <cem.yeniceri@digital.hmrc.gov.uk>
2023-01-11 13:08:12 +0000

    PODS-7932 copyright changes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,10 +22,10 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"      % "0.73.0",
     "com.vladsch.flexmark"    %  "flexmark-all"                 % "0.62.2",
     "de.flapdoodle.embed"     %  "de.flapdoodle.embed.mongo"    % "3.5.3",
-    "org.scalatestplus"       %% "scalacheck-1-17"            % "3.2.14.0",
-    "org.scalatestplus"       %% "mockito-4-6"                % "3.2.14.0",
-    "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0",
-    "org.scalatest"           %% "scalatest"                  % "3.2.14",
+    "org.scalatestplus"       %% "scalacheck-1-17"              % "3.2.14.0",
+    "org.scalatestplus"       %% "mockito-4-6"                  % "3.2.14.0",
+    "org.scalatestplus.play"  %% "scalatestplus-play"           % "5.1.0",
+    "org.scalatest"           %% "scalatest"                    % "3.2.14",
     "org.pegdown"             %  "pegdown"                      % "1.6.0",
     "com.github.tomakehurst"  %  "wiremock-jre8"                % "2.35.0"
   ).map(_ % "test")
